e07635f49935acdddecec0640fb69309c300c50f
Atlas666 <544871201@qq.com>
2018-10-28 11:21:09 +0800

    In this commit, the basic UI in ContactActivity has been implemented such as ToolBar, RecyclerView, FloatingActionButton. --2018/10/28

--- app/src/main/java/com/example/gg/ocontact/ContactActivity.java
+++ app/src/main/java/com/example/gg/ocontact/ContactActivity.java
@@ -1,10 +1,16 @@
 package com.example.gg.ocontact;
 
+import android.content.Context;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
@@ -36,14 +42,77 @@ public class ContactActivity extends AppCompatActivity {
             }
         });
 
-        // 联系人添加按钮
-        FloatingActionButton fab = findViewById(R.id.contact_add_contact_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        final Button btn_back = findViewById(R.id.contact_btn_back);
+        final EditText editText_search = findViewById(R.id.contact_edit_text_search);
+
+        // 搜索框返回 Button
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showToast("Add Contact clicked");
+                editText_search.setFocusable(false);
+                editText_search.setFocusableInTouchMode(false);
+
+                // 隐藏输入法
+                InputMethodManager imm = (InputMethodManager) editText_search.getContext().
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                if (imm != null) {
+                    imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+
+                btn_back.setVisibility(View.GONE);
             }
         });
+
+
+        // 搜索框 EditText
+        editText_search.setFocusableInTouchMode(false);
+
+        editText_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_back.setVisibility(View.VISIBLE);
+                editText_search.setFocusable(true);
+                editText_search.setFocusableInTouchMode(true);
+                editText_search.requestFocus();
+                editText_search.requestFocusFromTouch();
+
+                // 显示输入法
+                InputMethodManager imm = (InputMethodManager) editText_search.getContext().
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                if (imm != null) {
+                    imm.showSoftInput(editText_search, 0);
+                }
+            }
+        });
+
+
+
+        // 联系人添加 FAB -- !!! 跳转 EditActivity !!!
+        FloatingActionButton fab_add = findViewById(R.id.contact_fab_add_contact);
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("'fab_add' clicked");
+            }
+        });
+
+
+        // 回到最上方 FAB
+        FloatingActionButton fab_to_the_top = findViewById(R.id.contact_fab_to_the_top);
+        fab_to_the_top.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("'fab_to_the_top' clicked");
+            }
+        });
+
+
+        // 联系人列表 RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.contact_recyclerView);
+
 
     }
 
@@ -51,7 +120,7 @@ public class ContactActivity extends AppCompatActivity {
 
 
 
-    // 避免多次 add_contact_toast 多次重复提示
+    // 避免多次 add_contact_toast 多次重复提示 (辅助功能)
     private Toast toast;
     private void showToast(String msg){
         if (toast != null) {
@@ -63,5 +132,4 @@ public class ContactActivity extends AppCompatActivity {
             toast.show();
         }
     }
-
 }
